b0add9745e83e2d5e014fcf87ef4d1f67038561d
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-06-20 23:33:21 +0300

    Added supplier table to the database

---
+++ laravel/database/migrations/2023_06_20_202719_create_supplier_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('contact_person');
+            $table->string('telephone_number');
+            $table->string('email');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier');
+    }
+};
