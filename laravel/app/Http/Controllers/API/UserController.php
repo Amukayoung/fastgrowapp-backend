41b44ce927359f67e5596c4e3b739e5644223c71
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-06-26 22:44:26 +0300

    Added user post method

--- laravel/app/Http/Controllers/API/UserController.php
+++ laravel/app/Http/Controllers/API/UserController.php
@@ -21,7 +21,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userDetails = $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'user_name' => 'nullable|string',
+            'country_code' => 'nullable|string',
+            'telephone_number' => 'nullable|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user_info = User::Create([
+            'first_name'=>$userDetails['first_name'],
+            'last_name' => $userDetails['last_name'],
+            'user_name' => $userDetails['user_name'],
+            'country_code' => $userDetails['country_code'],
+            'telephone_number' => $userDetails['telephone_number'],
+            'email' => $userDetails['email'],
+            'password' => bcrypt($userDetails['password'])
+        ]);
+
+        $access_token = $user->createToken('my_access_token')->plainTextToken;
+        $reponse = [
+            'user' => $user,
+            'token' => $access_token
+        ];
+
+        return response($reponse, 201);
+
     }
 
     /**
